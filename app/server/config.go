5ab6400e710cbd54869fcf322215514db34f3fcd
alucard <knyazev.activ@yandex.ru>
2022-06-23 23:42:46 +0300

    Добавляет стили

--- app/server/config.go
+++ app/server/config.go
@@ -4,8 +4,8 @@ import "github.com/go-sql-driver/mysql"
 
 func GetConfig() mysql.Config {
 	return mysql.Config{
-		User:   "admin",
-		Passwd: "Password10$",
+		User:   "root",
+		Passwd: "we92kjkszp",
 		Net:    "tcp",
 		Addr:   "localhost:3306",
 		DBName: "models_db",
