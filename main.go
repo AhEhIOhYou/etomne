5f4116f61ac64402bfd46ea269bedd6b01a6d3e5
ahehiohyou <vladimirov_d1ma@mail.ru>
2022-06-24 17:56:32 +0300

    edit main app function

--- main.go
+++ main.go
@@ -1,32 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"etomne/app/controllers"
-	"log"
-
+	"etomne/app/server"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
+type app struct {
+	Router gin.Engine
+	Db     *sql.DB
+	Cache  map[string]interface{}
+}
+
 func main() {
+
 	router := gin.Default()
+	DB := server.Connect()
+	defer DB.Close()
 
 	router.StaticFile("favicon.ico", "assets/images/favicon.ico")
 	router.Static("assets/js/", "assets/js/")
 	router.Static("assets/css/", "assets/css/")
 	router.Static("upload/", "upload/")
 
-	routes(router)
-	err := router.Run(":8091")
+	Routes(router)
+
+	a := app{
+		Router: *router,
+		Db:     DB,
+	}
+
+	err := a.Router.Run(":8091")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func routes(r *gin.Engine) {
+func Routes(r *gin.Engine) {
 
 	r.LoadHTMLGlob("app/views/*")
 
-	r.Any("/", controllers.Index)
+	r.GET("/", controllers.Index)
 	r.NoRoute(controllers.NotFound)
 
 	r.GET("/models", controllers.Models)
@@ -37,7 +53,11 @@ func routes(r *gin.Engine) {
 
 	r.GET("/delete", controllers.Delete)
 
-	r.Any("/login", controllers.UserLogin)
-	r.Any("/reg", controllers.UserReg)
+	r.GET("/login", controllers.UserLogin)
+	r.POST("/login", controllers.UserLogin)
+
+	r.GET("/reg", controllers.UserReg)
+	r.POST("/reg", controllers.UserReg)
+
 	r.GET("/exit", controllers.UserLogout)
 }
