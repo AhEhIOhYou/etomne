42120af87a8ccaaa28823d5589a5011e6ebff364
ahehiohyou <vladimirov_d1ma@mail.ru>
2022-06-24 13:46:40 +0300

    edit config file

--- app/server/database.go
+++ app/server/database.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"log"
 )
 
@@ -9,9 +12,22 @@ func Connect() *sql.DB {
 
 	var db *sql.DB
 
-	cfg := GetConfig()
+	yfile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	var err error
+	data := make(map[string]string)
+	yaml.Unmarshal(yfile, &data)
+
+	cfg := mysql.Config{
+		User:   data["User"],
+		Passwd: data["Passwd"],
+		Net:    data["Net"],
+		Addr:   data["Addr"],
+		DBName: data["DBName"],
+	}
+
 	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
