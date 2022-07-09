f153b077e79c4464999908a3518af49b49e82a07
ahehiohyou <diman_vladimirov34@mail.ru>
2022-07-09 23:53:52 +0300

    refactor to DDD #5

--- interfaces/model_handler.go
+++ interfaces/model_handler.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Model struct {
@@ -96,4 +98,144 @@ func (m *Model) SaveModel(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusCreated, saveModel)
+}
+
+func (m *Model) UpdateModel(c *gin.Context) {
+	metadata, err := m.tk.ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	userId, err := m.rd.FetchAuth(metadata.TokenUuid)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+
+	user, err := m.userApp.GetUser(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "user not found, unauthorized")
+		return
+	}
+
+	modelId, err := strconv.ParseUint(c.Param("model_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "invalid request")
+		return
+	}
+
+	model, err := m.modelApp.GetModel(modelId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, err.Error())
+		return
+	}
+
+	if user.ID != model.UserID {
+		c.JSON(http.StatusUnauthorized, "you are not the owner of this model")
+		return
+	}
+
+	var updateModelErr = make(map[string]string)
+
+	title := c.PostForm("title")
+	description := c.PostForm("description")
+
+	if fmt.Sprintf("%T", title) != "string" || fmt.Sprintf("%T", description) != "string" {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json")
+	}
+
+	emptyModel := entities.Model{
+		Title:       title,
+		Description: description,
+	}
+
+	updateModelErr = emptyModel.Validate("update")
+	if len(updateModelErr) > 0 {
+		c.JSON(http.StatusUnprocessableEntity, updateModelErr)
+		return
+	}
+
+	// В разработке
+	//file, _ := c.FormFile("model_file")
+
+	/*
+		if file != nil {
+			model.ModelFile, err = m.fileUpload.UploadFile(file)
+
+			model.ModelFile = os.Getenv("DO_SPACES_URL") + model.ModelFile
+			if err != nil {
+				c.JSON(http.StatusUnprocessableEntity, gin.H{
+					"upload_err": err.Error(),
+				})
+				return
+			}
+		}
+	*/
+
+	model.Title = title
+	model.Description = description
+	model.UpdatedAt = time.Now()
+	updatedFood, dbUpdateErr := m.modelApp.UpdateModel(model)
+	if dbUpdateErr != nil {
+		c.JSON(http.StatusInternalServerError, dbUpdateErr)
+		return
+	}
+	c.JSON(http.StatusOK, updatedFood)
+}
+
+func (m *Model) GetAllModels(c *gin.Context) {
+	allModels, err := m.modelApp.GetAllModel()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, allModels)
+}
+
+func (m *Model) GetModelAndAuthor(c *gin.Context) {
+	modelId, err := strconv.ParseUint(c.Param("model_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "invalid request")
+		return
+	}
+	model, err := m.modelApp.GetModel(modelId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	user, err := m.userApp.GetUser(model.UserID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	foodAndUser := map[string]interface{}{
+		"model":  model,
+		"author": user.PublicUser(),
+	}
+	c.JSON(http.StatusOK, foodAndUser)
+}
+
+func (m *Model) DeleteModel(c *gin.Context) {
+	metadata, err := m.tk.ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	modelId, err := strconv.ParseUint(c.Param("model_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "invalid request")
+		return
+	}
+	_, err = m.userApp.GetUser(metadata.UserId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	err = m.modelApp.DeleteModel(modelId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, "model deleted")
 }
