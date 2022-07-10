67e35b59f9037fd0426c02f2404fafca6be5bdec
ahehiohyou <diman_vladimirov34@mail.ru>
2022-07-10 19:25:55 +0300

    token bugfix

--- backend/infrastructure/auth/token.go
+++ backend/infrastructure/auth/token.go
@@ -87,7 +87,6 @@ func TokenValid(r *http.Request) error {
 func VerifyToken(r *http.Request) (*jwt.Token, error) {
 	tokenString := ExtractToken(r)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		//Make sure that the token method conform to "SigningMethodHMAC"
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -102,7 +101,7 @@ func VerifyToken(r *http.Request) (*jwt.Token, error) {
 func ExtractToken(r *http.Request) string {
 	bearToken := r.Header.Get("Authorization")
 	strAtr := strings.Split(bearToken, " ")
-	if len(strAtr) > 2 {
+	if len(strAtr) > 1 {
 		return strAtr[1]
 	}
 	return ""
