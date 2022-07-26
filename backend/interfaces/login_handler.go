610a29a705b415f1e3bd05ed0572f2801efa967f
ahehiohyou <diman_vladimirov34@mail.ru>
2022-07-26 15:15:15 +0300

    add logout and refresh docs, update structs

--- backend/interfaces/login_handler.go
+++ backend/interfaces/login_handler.go
@@ -26,17 +26,37 @@ func NewAuthenticate(uApp application.UserAppInterface, rd auth.AuthInterface, t
 	}
 }
 
+type AuthUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserData struct {
+	ID           uint64 `json:"id"`
+	Name         string `json:"name"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RToken struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Tokens struct {
+}
+
 // Login doc
 // @Summary		Login user
 // @Tags		Authorization
 // @Accept 		json
 // @Produce		json
-// @Param 		data 	body 	  entities.AuthUser true "Auth user json"
-// @Success 	200 	{object}  entities.UserData
+// @Param 		data 	body 	  AuthUser true "Auth user data"
+// @Success 	200 	{object}  UserData
 // @Failure     422  {string} string  "invalid json provided"
 // @Failure     500  {string} string  "error"
 // @Router		/users/login [post]
 func (au *Authenticate) Login(c *gin.Context) {
+
 	var user *entities.User
 	var tokenErr = map[string]string{}
 
@@ -70,7 +90,7 @@ func (au *Authenticate) Login(c *gin.Context) {
 		return
 	}
 
-	var userData = entities.UserData{
+	var userData = UserData{
 		ID:           u.ID,
 		Name:         u.Name,
 		AccessToken:  ts.AccessToken,
@@ -80,30 +100,49 @@ func (au *Authenticate) Login(c *gin.Context) {
 	c.JSON(http.StatusOK, userData)
 }
 
+// Logout doc
+// @Summary		Logout user
+// @Tags		Authorization
+// @Accept 		json
+// @Produce		json
+// @Success 	200  {string} string  "successfully logged out"
+// @Failure     401  {string} string  "unauthorized"
+// @Failure     500  {string} string  "error"
+// @Router		/users/logout [post]
 func (au *Authenticate) Logout(c *gin.Context) {
 	metadata, err := au.tk.ExtractTokenMetadata(c.Request)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, "Unauthorized")
+		c.JSON(http.StatusUnauthorized, "unauthorized")
 		return
 	}
 	deleteErr := au.rd.DeleteTokens(metadata)
 	if deleteErr != nil {
-		c.JSON(http.StatusUnauthorized, deleteErr.Error())
+		c.JSON(http.StatusInternalServerError, deleteErr.Error())
 		return
 	}
-	c.JSON(http.StatusOK, "Successfully logged out")
+	c.JSON(http.StatusOK, "successfully logged out")
 }
 
+// Refresh doc
+// @Summary		Refresh user token
+// @Tags		Authorization
+// @Accept 		json
+// @Produce		json
+// @Param 		data  body 	  RToken true "Refresh token"
+// @Success 	200  {object} UserData
+// @Failure     401  {string} string  "unauthorized"
+// @Failure     403  {string} string  "error"
+// @Failure     422  {string} string  "error"
+// @Failure     500  {string} string  "error"
+// @Router		/users/refresh [post]
 func (au *Authenticate) Refresh(c *gin.Context) {
-	mapToken := map[string]string{}
-	if err := c.ShouldBindJSON(&mapToken); err != nil {
+	var rToken *RToken
+	if err := c.ShouldBindJSON(&rToken); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, err.Error())
 		return
 	}
 
-	refreshToken := mapToken["refresh_token"]
-
-	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(rToken.RefreshToken, func(token *jwt.Token) (interface{}, error) {
 		//Make sure that the token method conform to "SigningMethodHMAC"
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
@@ -124,13 +163,13 @@ func (au *Authenticate) Refresh(c *gin.Context) {
 	if ok && token.Valid {
 		refreshUuid, ok := claims["refresh_uuid"].(string)
 		if !ok {
-			c.JSON(http.StatusUnprocessableEntity, "Cannot get uuid")
+			c.JSON(http.StatusUnprocessableEntity, "cannot get uuid")
 			return
 		}
 
 		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
 		if err != nil {
-			c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+			c.JSON(http.StatusUnprocessableEntity, "error occurred")
 			return
 		}
 
@@ -152,11 +191,13 @@ func (au *Authenticate) Refresh(c *gin.Context) {
 			return
 		}
 
-		tokens := map[string]string{
-			"access_token":  ts.AccessToken,
-			"refresh_token": ts.RefreshToken,
+		var userData = UserData{
+			ID:           userId,
+			AccessToken:  ts.AccessToken,
+			RefreshToken: ts.RefreshToken,
 		}
-		c.JSON(http.StatusCreated, tokens)
+
+		c.JSON(http.StatusCreated, userData)
 	} else {
 		c.JSON(http.StatusUnauthorized, "refresh token expired")
 	}
